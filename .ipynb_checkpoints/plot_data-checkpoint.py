ceb246a93b1a27a6569f7ac4e99e2499ca0becda
Thorsten Heimes <thorstentheimes@gmail.com>
2018-02-22 10:25:16 +0100

    Removed hidden ipython folder

--- .ipynb_checkpoints/plot_data-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv('data/attendance.csv')
-df.plot('local_date', 'yes_rsvp_count')
-plt.title('PyData Meetup Attendance')
-plt.gcf().savefig('figures/attendance.png')
-
--- .ipynb_checkpoints/run_notebooks-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-jupyter nbconvert --to html PyData.ipynb
-jupyter nbconvert --to html SuperPython.ipynb
-
-python pydata_attendance.py
